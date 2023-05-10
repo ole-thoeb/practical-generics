f838d8f5766e15d3b323c4d244f29cae1f91ed10
Emil <s6embeot@uni-bonn.de>
2023-05-10 18:17:52 +0200

    Add comment and adjust parameter names

--- src/option.solution.ts
+++ src/option.solution.ts
@@ -6,15 +6,18 @@ class Some<T> {
         return this.value
     }
 
-    map<U>(f: (t: T) => U): Option<U> {
-        return Option.some(f(this.value))
+    map<U>(mapper: (t: T) => U): Option<U> {
+        return Option.some(mapper(this.value))
     }
 
-    flatMap<U>(f: (t: T) => Option<U>): Option<U> {
-        return f(this.value)
+    flatMap<U>(mapper: (t: T) => Option<U>): Option<U> {
+        return mapper(this.value)
     }
 
     flatten<U>(this: Option<Option<U>>): Option<U> {
+        // `this` type can't be `Some<Option<U>>` otherwise `flatten` is not callable on any `Option`,
+        // therefore we need to check here, even though we know we have a `Some`.
+        // Could also be solved with a cast.
         if (this.hasValue) {
             return this.value
         }
@@ -42,14 +45,16 @@ class None {
     }
 }
 
+const NONE_INSTANCE = new None()
+
 export type Option<T> = Some<T> | None
 
 export const Option = {
-    some<T>(t: T): Option<T> {
-        return new Some(t)
+    some<T>(value: T): Option<T> {
+        return new Some(value)
     },
     none(): Option<never> {
-        return new None()
+        return NONE_INSTANCE
     },
     fromNullable<T>(nullishValue: T): Option<NonNullable<T>> {
         if (nullishValue == null) {
